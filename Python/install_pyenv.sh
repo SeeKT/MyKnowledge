aa8756d1aebe22a5b4b18580473fae45baa6c2bc
Kosuke Toda <td49ksk@gmail.com>
2021-03-19 21:58:52 +0900

    update install python sh

--- Python/install_pyenv.sh
+++ Python/install_pyenv.sh
@@ -3,7 +3,7 @@
 # Written by K.Toda, 2021/3/19
 
 # Objective
-## To install pyenv and Python 3.7.10, 3.8.8 in Ubuntu 18
+## To install pyenv in Ubuntu 18
 
 # Reference
 ## https://qiita.com/123h4wk/items/d5aece92d1ee47ce603c
@@ -31,11 +31,3 @@ echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bashrc
 ## reload shell
 exec "$SHELL"
-
-# Install Python 3.7.10 and 3.8.8
-## install packages that is necessary for building python
-sudo -S apt-get update
-sudo -S apt-get install --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
-## install Python
-pyenv install 3.7.10
-pyenv install 3.8.8
